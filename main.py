9f3c10312d75026c17c012ceeacce4a4451f8b0a
ASTRO063 <manikanta.kodimala3@gmail.com>
2021-06-02 18:56:40 +0530

    divisionByTwo-feature is added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def addition(num1=2,num2=3):
     return num1+num2
 
-if __name__=="__main__":
-    print(addition())
     
+def divisionByTwo(additionValue = 6):
+    return additionValue//2
+
+if __name__=="__main__":
+    additionValue = addition()
+    print(divisionByTwo(additionValue))
